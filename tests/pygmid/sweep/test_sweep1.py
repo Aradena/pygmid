4440f4c80e2a35719285054355835088bf8b9544
aradena <maxime.morenas@gmail.com>
2026-01-09 19:39:22 +0100

    ready to try

--- tests/pygmid/sweep/test_sweep1.py
+++ tests/pygmid/sweep/test_sweep1.py
@@ -1,5 +1,7 @@
 from pygmid import sweep
 
-sweep.run("./tests/pygmid/sweep/config_xt018.cfg")
+# @pytest.mark.parametrize("config_file", [Path("tests/pygmid/sweep/config_xt018.cfg")])
+def mytest():
+    sweep.run("./tests/pygmid/sweep/config_xt018.cfg")
 
 #/home/m.morena/design/XFAB/XT018/pygmid/pygmid
