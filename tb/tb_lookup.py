df3814cbb7f97435a96458220907afb186e095d7
aradena <maxime.morenas@gmail.com>
2026-01-23 16:33:57 +0100

    storing result in a dict

--- tb/tb_lookup.py
+++ tb/tb_lookup.py
@@ -13,9 +13,10 @@ mpl.rcParams.update({"axes.grid" : True})
 mpl.use("TkAgg")
 
 #%% Load process data into pygmid lookup object
-NCH = lk('xt018_ne5.pkl')
-PCH = lk('xt018_pe5.pkl')
-
+# NCH = lk('xt018_ne5.pkl')
+# PCH = lk('xt018_pe5.pkl')
+NCH = lk('65nch.mat')
+PCH = lk('65pch.mat')
 
 # %% Gm over Id methodology applied to folded-cascode OTA
 
@@ -33,7 +34,8 @@ beta = 0.75*beta_max # first-order optimum
 kappa = 0.7 # conservative optimum
 
 # Channel length sweep
-L23 = L45 = L =  np.linspace(0.5,5,100)
+# L23 = L45 = L =  np.linspace(0.5,5,100)
+L23 = L45 = L =  np.linspace(0.06,1,100)
 gm_gds2 = NCH.look_up('GM_GDS', gm_id=gm_id, vds=0.2, L=L23)
 gm_gds3 = NCH.look_up('GM_GDS', gm_id=gm_id, vds=0.4, L=L23)
 gm_gds4 = PCH.look_up('GM_GDS', gm_id=gm_id, vds=0.4, L=L45).reshape(-1,1)
@@ -52,50 +54,41 @@ def fmt(x):
     # return rf"{s} \%" if plt.rcParams["text.usetex"] else f"{s} %"
     return rf"{s}"
 
-fig, ax = plt.subplots()
-levels= np.linspace(10,100,10)
-CS = ax.contour(L23, L45, L0_swept) #, levels)
-ax.set_ylabel(r"$L_{4,5}$ [$\mu$m]")
-ax.set_xlabel(r"$L_{2,3}$ [$\mu$m]")
-ax.set_title(r'Low frequency loop gain $L_0$ as a function of channel lengths')
-ax.clabel(CS, CS.levels, fmt=fmt, fontsize=10)
+# fig, ax = plt.subplots()
+# levels= np.linspace(10,100,10)
+# CS = ax.contour(L23, L45, L0_swept) #, levels)
+# ax.set_ylabel(r"$L_{4,5}$ [$\mu$m]")
+# ax.set_xlabel(r"$L_{2,3}$ [$\mu$m]")
+# ax.set_title(r'Low frequency loop gain $L_0$ as a function of channel lengths')
+# ax.clabel(CS, CS.levels, fmt=fmt, fontsize=10)
 # plt.show()
-plt.close()
+# plt.close()
 
-L23 = L45 = 2  # 300 < L0 < 400  (50 with book techno in 65 nm)
+# L23 = L45 = 2  # 300 < L0 < 400  (50 with book techno in 65 nm)
+L23 = L45 = 0.4  # L0 ~ 60
+
 
 gmb_gm3 = NCH.look_up('GMB_GM', gm_id=gm_id, vds=0.4, vsb=0.2, L=L23)
 gm_css3 = NCH.look_up('GM_CSS', gm_id=gm_id, vds=0.4, vsb=0.2, L=L23)
 cdd_css3 = NCH.look_up('CDD_CSS', gm_id=gm_id, vds=0.4, vsb=0.2, L=L23)
 cdd_w3 = NCH.look_up('CDD_CSS', gm_id=gm_id, vds=0.4, vsb=0.2, L=L23)
-cdd_w2 = NCH.look_up('CDD_CSS', gm_id=gm_id, vds=0.2, vsb=0.2, L=L23)
+cdd_w2 = NCH.look_up('CDD_CSS', gm_id=gm_id, vds=0.2, L=L23) # vsb=0.2,
 fp2 = 1/2/np.pi*gm_css3*(1+gmb_gm3)/(1+2*cdd_css3*2*(cdd_w2/cdd_w3))
-print(f'Non dominant pole fp2 = {Float(fp2):!.1h}Hz') #%.2E Hz' % fp2)
+print(f'Non dominant pole fp2 = {Float(fp2):!.2h}Hz') #%.2E Hz' % fp2)
 
 # %% Design for a given settling time & noise budget, minimise power dissipation
-s.ts = 100e-9
+# s.ts = 100e-9
+s.ts = 5e-9
 s.ed = 0.1e-2
 s.fu1 = 1/2/np.pi*np.log(1/s.ed)/s.ts
 print("fp2/s.fu1 = %.2f gives a phase margin of about "%(fp2/s.fu1))
 
-s.vod = 50 # total integrated noise
+s.vod = 400e-6 # total integrated noise
 s.T = 300 # Kelvin
 s.fan_out = 0.5
 
 def folded_cascode(s, d):
-    class mos: pass
-    m1 = mos()
-    p1 = mos()
-    m1.gm_id1 = []
-    m1.id1 = []
-    m1.wTi = []
-    m1.Cgg1 = []
-    m1.Cgd1 = []
-    m1.kappa = []
-    m1.CLtot = []
-    m1.CF = []
-    m1.CS = []
-    m1.gamma = []
+    m1 = {}
 
     # Extra hypothesis
     gamma1 = gamma5 = gamma2 = 0.7
@@ -103,8 +96,10 @@ def folded_cascode(s, d):
     gm1_gm3 = 1
 
     # 2D sweep for finding beta with a physical reality
-    for beta in d.beta[:-1]: # Exclude last point for removing issues with interpolation to close to upper boundary
+    for beta in d.beta:
+        # Exclude last point for removing issues with interpolation to close to upper boundary
 
+        gm_id1 = d.gm_id1
         # Excess noise factor
         alpha = 2*gamma1*(1+gamma5/gamma1*gm_id5/d.gm_id1+2*gamma2/gamma1*gm_id2/d.gm_id1)
 
@@ -138,26 +133,35 @@ def folded_cascode(s, d):
 
         # Actual beta & physical design point
         beta_actual = CF/(CF+CS+Cin)
-        gm_id1_physical = interp1(beta_actual, d.gm_id1, kind='nearest')(beta)
+        gm_id1_physical = interp1(beta_actual, d.gm_id1, kind='nearest', fill_value="extrapolate")(beta)
         physical_index = np.where(d.gm_id1==gm_id1_physical)
 
+
         # Save
-        m1.gm_id1.append(gm_id1_physical)
-        m1.id1.append(id1[physical_index])
-        m1.wTi.append(wTi[physical_index])
-        m1.Cgg1.append(Cgg1[physical_index])
-        m1.Cgd1.append(Cgd1[physical_index])
-        m1.kappa.append(kappa[physical_index])
-        m1.CLtot.append(CLtot[physical_index])
-        m1.CF.append(CF[physical_index])
-        m1.CS.append(CS[physical_index])
+        for var in ['gm_id1', 'id1', 'wTi', 'Cgg1', 'Cgd1', 'kappa', 'CLtot', 'CF', 'CS', 'W', 'alpha']:
+            if var not in m1.keys():
+                m1[var] = np.array([])
+            m1[var] = np.append(m1[var], locals()[var][physical_index])
+
+        pass
+        # m1.gm_id1.append(gm_id1_physical)
+        # m1.id1.append(id1[physical_index])
+        # m1.wTi.append(wTi[physical_index])
+        # m1.Cgg1.append(Cgg1[physical_index])
+        # m1.Cgd1.append(Cgd1[physical_index])
+        # m1.kappa.append(kappa[physical_index])
+        # m1.CLtot.append(CLtot[physical_index])
+        # m1.CF.append(CF[physical_index])
+        # m1.CS.append(CS[physical_index])
+        # m1.W.append(W[physical_index])
 
         # Check hypothesis
-        gamma = NCH.look_up('STH_GM', gm_id=gm_id1_physical, L=d.L1)/4/sp.constants.Boltzmann/s.T  #  vgs=vgs1
-        m1.gamma.append(gamma)
+        # wrong formula ? gamma = NCH.look_up('STH_GM', gm_id=gm_id1_physical, L=d.L1)/4/sp.constants.Boltzmann/s.T  #  vgs=vgs1
+        # m1.gamma.append(gamma)
+        # plt.plot(np.array(m1.gm_id1), np.array(m1.gm_id1).reshape(-1, 1) * np.array(m1.id1))
 
     plt.figure()
-    plt.plot(m1.gm_id1, m1.id1, label='L = %.1f um'%d.L1)
+    plt.plot(m1['gm_id1'], m1['id1'], label='L = %.2f um'%d.L1)
     plt.ylabel(r"$I_D$ [mA]")
     plt.xlabel(r"$g_m/I_D$ [S/A]")
     plt.title(r'$I_D$ vs. $g_m/I_D$ for varying $L$')
